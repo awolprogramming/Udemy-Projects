455d394120f0f781901785be8c2ca8284bbab89b
Adrian Wolan Backend <157213675+awolprogramming@users.noreply.github.com>
2024-02-02 17:08:15 +0100

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,48 @@
+﻿namespace Udemy
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! \nWrite the first number:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+        
+            Console.WriteLine("Write the second number:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What do you want to do? \n [A]dd numbers \n [S]ubtract numbers \n [M]ultiply numbers");
+            string chosenProperty = Console.ReadLine().ToLower();
+            string chosenOperator;
+            int result;
+
+            if (chosenProperty != "a" && chosenProperty != "s" && chosenProperty != "m") {
+                Console.WriteLine("Wrong property!");
+                return;
+            }
+
+            switch (chosenProperty)
+            {
+                case "a":
+                    chosenOperator = "+";
+                    result = firstNumber + secondNumber;
+                    break;
+                case "s":
+                    chosenOperator = "/";
+                    result = firstNumber / secondNumber;
+                    break;
+                case "m":
+                    chosenOperator = "*";
+                    result = firstNumber * secondNumber;
+                    break;
+                default:
+                    chosenOperator = "+";
+                    result = firstNumber + secondNumber;
+                    break;
+            }
+
+            Console.WriteLine(firstNumber + " " + chosenOperator + " " + secondNumber + " = " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
